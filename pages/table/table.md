06f15e49e7dc8600a9fd1dd3c100ebf53cf03147
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-07-04 09:18:13 -0400

    update table py table md

--- pages/table/table.md
+++ pages/table/table.md
@@ -1,8 +1,3 @@
 # **Breweries List**{: .color-primary}
 
-<|{data}|table|
-    columns=["Name", "Street", "City", "State", "Postal Code", "Country", "Type", "Google Maps Link", "Website Link"]|
-    page_size=10|
-    page_size_options=[10, 50, 100]|
-    filter=True|
->
+<|{data}|table|columns=columns|page_size=10|page_size_options=[10, 50, 100]|filter=True|>
--- pages/table/table.py
+++ pages/table/table.py
@@ -5,9 +5,24 @@ from ..data.data import fetch_data
 # Fetch the data
 data = fetch_data()
 
-# We need to add Google Maps and website links to the data
-data['Google Maps Link'] = data.apply(lambda row: "Google Maps Link: https://www.google.com/maps/search/?api=1&query=" + str(row['Latitude']) + "," + str(row['Longitude']), axis=1)
-data['Website Link'] = data.apply(lambda row: f"[{row['Name']} Website]({row['Website']})" if row['Website'] else "No Website", axis=1)
+# Remove the original website URL column
+data = data.drop(columns=['Longitude'])
+data = data.drop(columns=['Latitude'])
+data = data.drop(columns=['Google Maps Link'])
+#data = data.drop(columns=['Website'])
+
+# Define the columns for the table
+columns = [
+    {"field": "Name", "title": "Name"},
+    {"field": "Street", "title": "Street"},
+    {"field": "City", "title": "City"},
+    {"field": "State", "title": "State / Province"},
+    {"field": "Postal Code", "title": "Postal Code"},
+    {"field": "Country", "title": "Country"},
+    {"field": "Type", "title": "Type"},
+    {"field": "Google Maps", "title": "Google Maps", "type": "markdown"},
+    {"field": "Website Link", "title": "Website", "type": "markdown"},
+]
 
 # Create a Markdown control and set the content to the Taipy table syntax
 table_md = Markdown("pages/table/table.md")
