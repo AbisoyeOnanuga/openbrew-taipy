5dea0522eb661163484a339969d6c94a98045956
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-07-11 23:30:49 -0400

    update xhtml py

--- pages/table/xhtml_table.py
+++ pages/table/xhtml_table.py
@@ -4,10 +4,13 @@ from ..data.data import fetch_data
 data = fetch_data()
 
 # Your logic to drop columns and manipulate data
+# Generate HTML Links
+data['Google Maps'] = data.apply(lambda row: f"<a href='https://www.google.com/maps/search/?api=1&query={row['Latitude']},{row['Longitude']}' target='_blank'>View Map</a>", axis=1)
+data['Website Link'] = data.apply(lambda row: f"<a href='{row['Website']}' target='_blank'>{row['Name']} Website</a>" if row['Website'] else "No Website", axis=1)
 data = data.drop(columns=['Longitude', 'Latitude', 'Website'])
 #data = data.drop(columns=['Google Maps Link'])
 
-table_rows = ''.join([f"<tr><td>{row['Name']}</td><td>{row['Street']}</td><td>{row['City']}</td><td>{row['State']}</td><td>{row['Postal Code']}</td><td>{row['Country']}</td><td>{row['Type']}</td><td>{row['Google Maps']}</td><td>{row['Website Link']}</td></tr>" for index, row in data.iterrows()])
+table_rows = ''.join([f"<tr><td>{row['Name']}</td><td>{row['Street']}</td><td>{row['City']}</td><td>{row['State']}</td><td>{row['Postal Code']}</td><td>{row['Country']}</td><td>{row['Type']}</td><td>{row['Website Link']}</td><td>{row['Google Maps Link']}</td></tr>" for index, row in data.iterrows()])
 table_html = f"<table>{table_rows}</table>"
 
 # Define the XHTML page
