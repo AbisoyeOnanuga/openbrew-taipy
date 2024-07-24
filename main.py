541d2cc6f2c8958d5c77f3b405c21fe2c08e47e0
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-07-24 18:18:53 -0400

    update main py

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import taipy as tp
 
 # Import the Markdown pages
 from pages.root import root, selected_location, selector_location
-#from pages.table.table import table_md
-from pages.table.xhtml_table import table
-#from pages.dashboard.dashboard import dashboard_md
-from pages.dashboard.xhtml_dashboard import dashboard
+from pages.table.table import table_md
+from pages.dashboard.dashboard import dashboard_md
 from pages.map.map import map_md
 
 # Define the pages dictionary with the Markdown pages
 pages = {
     '/': root,
-    "List": table,
-    "Dashboard": dashboard,
+    "List": table_md,
+    "Dashboard": dashboard_md,
     "Map": map_md,
 }
 
